3771b290359739f6f15761724dc5e7f9f2b3281c
Neutoopia <nuetoopia@gmail.com>
2026-06-27 15:31:04 +0300

    add turso db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,14 @@
 import type { Config } from "drizzle-kit";
+import * as dotenv from 'dotenv';
+
+dotenv.config({path: ".env.local"});
 
 export default {
   schema:    "./db/schema.ts",
   out:       "./db/migrations",
-  dialect:   "sqlite",
+  dialect:   "turso",
   dbCredentials: {
     url: process.env.DATABASE_URL ?? "file:./dev.db",
+    authToken: process.env.DATABASE_AUTH_TOKEN!,
   },
 } satisfies Config;
